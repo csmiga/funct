8ec652556c48c335c1e77e7f78fd2e7c8bec7b5b
csmiga <cesmiga@gmail.com>
2020-08-28 20:57:04 +0000

    moved a few lines around

--- funct.sh
+++ funct.sh
@@ -9,8 +9,8 @@ File:    funct.sh
 END
 
 declare CONFIG=$HOME/Projects/funct/conf/funct.conf
-declare BIN=/root/Projects/venv3/bin/behave
 declare BEHAVIOR=/root/Projects/funct/lib/plutotv.features/
+declare BIN=/root/Projects/venv3/bin/behave
 declare PROCESS=$(pgrep behave)
 
 function funct_start () {
