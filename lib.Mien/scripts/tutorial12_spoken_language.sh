71160c9b38de7a92e818989d2f900799dcf5fe86
csmiga <cesmiga@gmail.com>
2020-09-09 21:59:33 +0000

    lastest files

---
+++ lib.Mien/scripts/tutorial12_spoken_language.sh
@@ -0,0 +1,8 @@
+#! /usr/bin/env bash
+#
+# Tutorial 12: Use another Spoken Language
+# https://jenisys.github.io/behave.example/tutorials/tutorial12.html
+# 
+# Goal: Use another spoken language for testing (other than English)
+
+behave ../features/tutorial12_spoken_language.feature
