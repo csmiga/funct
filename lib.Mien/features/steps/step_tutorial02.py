1dca261007ec2d7a05cade3a47909f52612b259d
csmiga <cesmiga@gmail.com>
2020-09-09 15:44:55 +0000

    support updates

--- lib.Mien/features/steps/step_tutorial02.py
+++ lib.Mien/features/steps/step_tutorial02.py
@@ -4,7 +4,7 @@
 # ----------------------------------------------------------------------------
 # STEPS:
 # ----------------------------------------------------------------------------
-from behave   import given, when, then
+from behave import given, when, then
 from hamcrest import assert_that, equal_to, is_not
 
 @given('the ninja has a {achievement_level}')
@@ -22,3 +22,29 @@ def step_attacked_by(context, opponent):
 @then('the ninja should {reaction}')
 def step_the_ninja_should(context, reaction):
     assert_that(reaction, equal_to(context.ninja_fight.decision()))
+
+# ----------------------------------------------------------------------------
+# PROBLEM DOMAIN:
+# ----------------------------------------------------------------------------
+class NinjaFight(object):
+    """
+    Domain model for ninja fights.
+    """
+    # pylint: disable=R0903
+
+    def __init__(self, with_ninja_level=None):
+        self.with_ninja_level = with_ninja_level
+        self.opponent = None
+
+    def decision(self):
+        """
+        Business logic how a Ninja should react to increase his survival rate.
+        """
+        assert self.with_ninja_level is not None
+        assert self.opponent is not None
+        if self.opponent == "Chuck Norris":
+            return "run for his life"
+        if "black-belt" in self.with_ninja_level:
+            return "engage the opponent"
+        else:
+            return "run for his life"
