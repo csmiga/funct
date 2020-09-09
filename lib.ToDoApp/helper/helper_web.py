61d4d3e71d7827f8cdd04a9f6123fe8f30a315bb
csmiga <cesmiga@gmail.com>
2020-09-09 00:15:58 +0000

    updated code

--- lib.ToDoApp/helper/helper_web.py
+++ lib.ToDoApp/helper/helper_web.py
@@ -1,19 +1,31 @@
-from selenium import webdriver
-from helper.helper_base import HelperFunc
-
-caps = {}
-
-def get_browser(browser, browser_version, platform, user_name, access_key):
-    remote_url = "https://" + user_name + ":" + access_key + "@hub.lambdatest.com/wd/hub"
-    caps['name'] = "[LambdaTest] [Behave] ToDo Application using Behave and Selenium"
-    caps['build'] = "[LambdaTest] [Behave] ToDo Application using Behave and Selenium"
-    caps['browserName'] = browser
-    caps['version'] = browser_version
-    caps['platform'] = platform
-    caps['network'] = True
-    caps['visual'] = True
-    caps['video'] = True
-    caps['console'] = True
-    if browser == "chrome":
-        # return Web(webdriver.Chrome())
-        return HelperFunc(webdriver.Remote(command_executor=remote_url, desired_capabilities=caps))
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+ 
+class HelperFunc(object):
+    __TIMEOUT = 10
+ 
+    def __init__(self, driver):
+        super(HelperFunc, self).__init__()
+        self._driver_wait = WebDriverWait(driver, HelperFunc.__TIMEOUT)
+        self._driver = driver
+ 
+    def open(self, url):
+        self._driver.get(url)
+ 
+    def maximize(self):
+        self._driver.maximize_window()        
+        
+    def close(self):
+        self._driver.quit()
+        
+    # Helper functions that are used to identify the web locators in Selenium Python tutorial  
+ 
+    def find_by_xpath(self, xpath):
+        return self._driver_wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
+ 
+    def find_by_name(self, name):
+        return self._driver_wait.until(EC.visibility_of_element_located((By.NAME, name)))
+ 
+    def find_by_id(self, id):
+        return self._driver_wait.until(EC.visibility_of_element_located((By.ID, id)))
