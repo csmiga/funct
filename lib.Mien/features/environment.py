86d914676ba800222f65cbb1b6b714cb0ec8fd72
csmiga <cesmiga@gmail.com>
2020-09-09 14:04:26 +0000

    new files

--- lib.Mien/features/environment.py
+++ lib.Mien/features/environment.py
@@ -1,28 +1,11 @@
 # -- ASSUMING: tags @browser.chrome or @browser.any are used.
 # BETTER: Use Fixture for this example.
 
-from behave.fixture import use_fixture_by_tag
-from configparser import ConfigParser
-from helper.helper_web import get_browser
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-import os
-import time
-
-caps = {}
+#def before_all(context):
+#    pass
  
-def before_all(context):
-    caps['browserName'] = context.config.userdata['browser']
-    caps['version'] = context.config.userdata['browser_version']
-    caps['platform'] = context.config.userdata['platform']
- 
-    helper_func = get_browser(caps['browserName'], caps['version'], caps['platform'], context.config.userdata['name'],
-                  context.config.userdata['app_key'])
-    context.helperfunc = helper_func
- 
-def after_all(context):
-    context.helperfunc.close()
+#def after_all(context):
+#    pass
 
 #def before_step(context, step)
 #    pass
--- lib.Mien/helper/remote_url.py
+++
@@ -1,8 +0,0 @@
-def get_browser(browser, browser_version, platform, user_name, access_key):
-    remote_url = "https://" + user_name + ":" + access_key + "@hub.lambdatest.com/wd/hub"
-……………………………………………………………………………………………………………………………….
-……………………………………………………………………………………………………………………………….
-if browser == "chrome":
-        # return Web(webdriver.Chrome())
-        return HelperFunc(webdriver.Remote(command_executor=remote_url, desired_capabilities=caps))
-
