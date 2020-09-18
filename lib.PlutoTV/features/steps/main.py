d6b767e5082207ab6e3451d92b6d707eb6fd797c
csmiga <cesmiga@gmail.com>
2020-09-18 18:28:54 +0000

    updates

--- lib.PlutoTV/features/steps/main.py
+++ lib.PlutoTV/features/steps/main.py
@@ -5,32 +5,27 @@ from behave import *
 
 @given('firefox web browser is installed')
 def step_impl(context):
-    if os.path.isfile('/usr/bin/firefox'):
-        return "pass"
-    else:
-        return "fail"
+    pass
 
 @when('pluto.tv is reachable')
 def step_impl(context):
-    #assert True is not False
-    #assert context.failed is False
-    pass
+    context.browser.get('http://www.google.com')
 
 @then('test pluto.tv main page')
 def step_impl(context):
-    
+    assert context.browser.title == "Google"
 
 
-@then('test pluto.tv main page')
-def step_impl(context):
-    option = webdriver.FirefoxOptions()
-    option.add_argument('--headless')
-    option.binary_location = '/usr/bin/firefox'
-
-    browser = webdriver.Firefox(firefox_options=option)
-    browser.get('https://pluto.tv/')
-
-    print(browser.page_source)
-    #print("Title: %s" % browser.title)
-    time.sleep(3)
-    browser.quit()
+#@then('test pluto.tv main page')
+#def step_impl(context):
+#    option = webdriver.FirefoxOptions()
+#    option.add_argument('--headless')
+#    option.binary_location = '/usr/bin/firefox'
+#
+#    browser = webdriver.Firefox(firefox_options=option)
+#    browser.get('https://pluto.tv/')
+#
+#    print(browser.page_source)
+#    #print("Title: %s" % browser.title)
+#    time.sleep(3)
+#    browser.quit()
