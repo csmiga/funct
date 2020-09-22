f406ffdebe1055ce13b37094d56b3cbaba58f07e
csmiga <cesmiga@gmail.com>
2020-09-22 03:08:37 +0000

    altered directory path

--- funct.sh
+++ funct.sh
@@ -9,7 +9,7 @@ File:    funct.sh
 END
 
 declare CONFIG=$HOME/Projects/funct/conf/funct.conf
-declare FEATURE=/root/Projects/funct/lib.plutotv/features/
+declare FEATURE=/root/Projects/funct/lib/features/
 declare BIN=/root/Projects/venv3/bin/behave
 declare PROCESS=$(pgrep behave)
 
